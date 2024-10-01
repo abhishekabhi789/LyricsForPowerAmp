f3d3e23b3be9bf2c34dd8b5d73e00a91e2909fc5
Abhishek <abhishekabhi9240@gmail.com>
2024-10-01 07:52:44 +0530

    dependencies updated, AGP updated

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -57,25 +57,25 @@ android {
 
 dependencies {
     implementation(project(":poweramp_api_lib"))
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.8.1")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.9.0")
     implementation("com.google.code.gson:gson:2.11.0")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.3")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.6")
     implementation("androidx.core:core-splashscreen:1.0.1")
-    implementation("androidx.activity:activity-compose:1.9.0")
-    implementation(platform("androidx.compose:compose-bom:2024.06.00"))
+    implementation("androidx.activity:activity-compose:1.9.2")
+    implementation(platform("androidx.compose:compose-bom:2024.09.02"))
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
     implementation("androidx.compose.material:material-icons-extended")
-    implementation("androidx.navigation:navigation-compose:2.7.7")
-    implementation("androidx.compose.foundation:foundation-android:1.6.8")
-    androidTestImplementation(platform("androidx.compose:compose-bom:2024.06.00"))
+    implementation("androidx.navigation:navigation-compose:2.8.1")
+    implementation("androidx.compose.foundation:foundation-android:1.7.2")
+    androidTestImplementation(platform("androidx.compose:compose-bom:2024.09.02"))
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
     //lifecycle
-    val lifeCycleVersion = "2.8.3"
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.3")
+    val lifeCycleVersion = "2.8.6"
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.6")
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:$lifeCycleVersion")
 }
