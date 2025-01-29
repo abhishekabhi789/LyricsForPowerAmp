196f82b0f8092c95f6edcbaa1498a77946aa5f78
Abhishek <abhishekabhi9240@gmail.com>
2025-01-29 15:36:56 +0530

    imporved filter input field accessibility

--- app/src/main/java/io/github/abhishekabhi789/lyricsforpoweramp/ui/components/TextInputWithChips.kt
+++ app/src/main/java/io/github/abhishekabhi789/lyricsforpoweramp/ui/components/TextInputWithChips.kt
@@ -45,6 +45,8 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.semantics.contentDescription
+import androidx.compose.ui.semantics.semantics
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -153,6 +155,7 @@ fun TextInputWithChips(
                 modifier = Modifier
                     .widthIn(min = 20.dp)
                     .focusRequester(focusRequester)
+                    .semantics { contentDescription = fieldLabel }
             )
         }
         if (chipList.isNotEmpty() || value.isNotEmpty()) {
