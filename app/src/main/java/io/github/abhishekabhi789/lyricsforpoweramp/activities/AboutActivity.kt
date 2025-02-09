66a28f5b68c887a4b54e847db66b9ef8cb118a2a
Abhishek <abhishekabhi9240@gmail.com>
2025-02-09 12:46:34 +0530

    fixed about activity theme, and updated app screenshot

--- app/src/main/java/io/github/abhishekabhi789/lyricsforpoweramp/activities/AboutActivity.kt
+++ app/src/main/java/io/github/abhishekabhi789/lyricsforpoweramp/activities/AboutActivity.kt
@@ -4,15 +4,27 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.lifecycle.viewmodel.compose.viewModel
 import io.github.abhishekabhi789.lyricsforpoweramp.ui.about.AboutScreen
 import io.github.abhishekabhi789.lyricsforpoweramp.ui.theme.LyricsForPowerAmpTheme
+import io.github.abhishekabhi789.lyricsforpoweramp.utils.AppPreference
+import io.github.abhishekabhi789.lyricsforpoweramp.viewmodels.MainActivityViewModel
 
 class AboutActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         enableEdgeToEdge()
         super.onCreate(savedInstanceState)
         setContent {
-            LyricsForPowerAmpTheme {
+            val viewModel: MainActivityViewModel = viewModel()
+            LaunchedEffect(Unit) {
+                val preferredTheme = AppPreference.getTheme(applicationContext)
+                viewModel.updateTheme(preferredTheme)
+            }
+            val appTheme by viewModel.appTheme.collectAsState()
+            LyricsForPowerAmpTheme(useDarkTheme = AppPreference.isDarkTheme(theme = appTheme)) {
                 AboutScreen(onFinish = ::finish)
             }
         }
